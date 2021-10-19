38b5ab0d9731c2e3cd07209f21b941b745ce4810
Angel Miguel Peralta <angelmiguelperalta@Angels-MacBook-Pro.local>
2021-10-18 21:36:28 -0400

    This is a first test commit

---
+++ src/main/java/com/psu/ist412/team2/projectplannerwebsite/ProjectPlannerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.psu.ist412.team2.projectplannerwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlannerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlannerWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psu/ist412/team2/projectplannerwebsite/ProjectPlannerWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psu.ist412.team2.projectplannerwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
