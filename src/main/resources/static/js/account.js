449639ed5d40a295a0cc056143684d3d49c4c00f
Angel Miguel Peralta <angelmiguelperalta@Angels-MacBook-Pro.local>
2021-12-08 23:20:51 -0500

    Finished Task creation implementation.

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,54 +1,54 @@
-$(".form-add-to-cart").on("submit", function (e) {
-  e.preventDefault();
-  var spinner = $("#div-add-to-cart-spinner-container");
-  spinner.fadeIn(250);
-  $.ajax({
-    url: $(this).attr("action"),
-    type: "POST",
-    dataType: "json",
-    data: $(this).serialize(),
-    success: function (response) {
-      spinner.fadeOut(250);
-      var inCartAmountElement = $("#div-in-cart-" + response.requestedItemSku);
-      var cartRequestedItemTotal = response.cartRequestedItemTotal;
+// $(".form-add-to-cart").on("submit", function (e) {
+//   e.preventDefault();
+//   var spinner = $("#div-add-to-cart-spinner-container");
+//   spinner.fadeIn(250);
+//   $.ajax({
+//     url: $(this).attr("action"),
+//     type: "POST",
+//     dataType: "json",
+//     data: $(this).serialize(),
+//     success: function (response) {
+//       spinner.fadeOut(250);
+//       var inCartAmountElement = $("#div-in-cart-" + response.requestedItemSku);
+//       var cartRequestedItemTotal = response.cartRequestedItemTotal;
 
-      inCartAmountElement.find(".span-item-total").text(cartRequestedItemTotal);
+//       inCartAmountElement.find(".span-item-total").text(cartRequestedItemTotal);
 
-      if (cartRequestedItemTotal > 0) {
-        inCartAmountElement.removeClass("d-none");
-      }
-      $("#span-cart-total-items").text(response.cartTotalItems);
+//       if (cartRequestedItemTotal > 0) {
+//         inCartAmountElement.removeClass("d-none");
+//       }
+//       $("#span-cart-total-items").text(response.cartTotalItems);
 
-      var addToCartResponseElement = $("#div-add-to-cart-response");
-      addToCartResponseElement.find("span").text(response.addToCartResponse);
+//       var addToCartResponseElement = $("#div-add-to-cart-response");
+//       addToCartResponseElement.find("span").text(response.addToCartResponse);
 
-      if (!response.isSuccess) {
-        addToCartResponseElement.css({ opacity: 0, display: "flex" }).animate(
-          {
-            opacity: 1,
-          },
-          1000,
-          function () {
-            addToCartResponseElement.addClass("attention");
-            setTimeout(function () {
-              addToCartResponseElement.fadeOut(1000);
-            }, 7000);
-          }
-        );
-      } else {
-        inCartAmountElement.addClass("change");
-        setTimeout(function () {
-          inCartAmountElement.removeClass("change");
-        }, 3000);
-      }
-    },
-    error: function () {
-      spinner.fadeOut(250);
-      alert("Error while requesting. Please try again later.");
-    },
-  });
-  $(this).trigger("reset");
-});
+//       if (!response.isSuccess) {
+//         addToCartResponseElement.css({ opacity: 0, display: "flex" }).animate(
+//           {
+//             opacity: 1,
+//           },
+//           1000,
+//           function () {
+//             addToCartResponseElement.addClass("attention");
+//             setTimeout(function () {
+//               addToCartResponseElement.fadeOut(1000);
+//             }, 7000);
+//           }
+//         );
+//       } else {
+//         inCartAmountElement.addClass("change");
+//         setTimeout(function () {
+//           inCartAmountElement.removeClass("change");
+//         }, 3000);
+//       }
+//     },
+//     error: function () {
+//       spinner.fadeOut(250);
+//       alert("Error while requesting. Please try again later.");
+//     },
+//   });
+//   $(this).trigger("reset");
+// });
 
 function addNewList() {
   var newListTemplate = `<div class="list">
@@ -141,15 +141,18 @@ function addNewTask(event) {
         var spinner = $("#div-add-to-cart-spinner-container");
         spinner.fadeIn(250);
 
+        var inputNewTaskNameParentParentParent =
+          inputNewTaskName.parentNode.parentNode.parentNode;
         $.ajax({
-          url: `/account/add-new-tasks-list/${inputNewListName.value}`,
+          url: `/account/add-new-task/tasks-list/${
+            inputNewTaskNameParentParentParent.getElementsByTagName("h3")[0]
+              .innerHTML
+          }/task/${inputNewTaskName.value}`,
           type: "POST",
           dataType: "json",
           success: function (response) {
             spinner.fadeOut(250);
 
-            var inputNewTaskNameParentParentParent =
-              inputNewTaskName.parentNode.parentNode.parentNode;
             inputNewTaskName.outerHTML = inputNewTaskName.value;
             inputNewTaskNameParentParentParent.innerHTML += `<button class="add-card-btn btn">Add a card</button>`;
             $(".add-card-btn").each(function () {
